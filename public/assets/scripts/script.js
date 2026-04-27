9ef8e03286531971b8d2da963fd5e6847d0295da
MaartentePoele <maarten.te.poele@hva.nl>
2026-04-27 11:05:53 +0200

    #4 Post interactie met loading states en client side JS

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -1,54 +1,30 @@
 const giftForms = document.querySelectorAll("form");
-// const newButton = document.querySelector("form button");
+const giftCounter = document.querySelector(".wishlist-counter")
 
 giftForms.forEach((giftForm) => {
-  // Als er op de submit button wordt geklikt ...
   giftForm.addEventListener("submit", async function (event) {
-    // Voorkom de standaard submit van de browser
-    // Let op: hiermee overschrijven we de default Loading state van de browser...
     event.preventDefault();
 
     const formButton = giftForm.querySelector("button");
-
-    console.log("gift submitted")
-
-    //Loading state tonen:
     formButton.classList.add("loading");
-    // formButton.textContent = "loading...";
 
-    //formdata voorbereiden:
     let formData = new FormData(giftForm);
 
-    // Data fetchen:
-    // Doe een fetch naar de server, net als hoe de browser dit normaal zou doen
-    // Gebruik daarvoor het action en method attribuut van het formulier
-    // Stuur de formulierelementen mee
     const response = await fetch(giftForm.action, {
-      method: giftForm.method, //POST dus
-      body: new URLSearchParams(formData), // <<< Dit moet omdat server.js anders niet met de formulier data kan werken
+      method: giftForm.method,
+      body: new URLSearchParams(formData),
     });
 
-    // Data verwerken:
-    // Jouw server.js geeft data terug als het posten goed gaat
     const responseData = await response.text();
-
-    // Normaal zou de browser die HTML parsen en weergeven.
-    // Maar omdat we dit nu in client-side JS doen moeten we dit zelf doen:
-    // Parse de nieuwe HTML en maak onderwater een nieuw Document Object Model aan
     const parser = new DOMParser();
     const responseDOM = parser.parseFromString(responseData, "text/html");
 
-    // Zoek in de onderwater DOM de nieuwe state op
-    const newState = responseDOM.querySelector("form button");
+    const buttonNewState = responseDOM.querySelector("form button");
+    const counterNewState = responseDOM.querySelector(".wishlist-counter");
 
-    // Overschrijf de HTML met de nieuwe HTML
-    // We gaan de nieuwe state toevoegen aan de DOM, aan de scorelijst in de ol
-    formButton.innerHTML = newState.innerHTML;
+    formButton.innerHTML = buttonNewState.innerHTML;
+    giftCounter.innerHTML = counterNewState.innerHTML;
 
-    // Loading state weghalen
-    // Nu kan je waarschijnlijk de Loading state vervangen door een Success state
-    console.log("Loading state weghalen");
     formButton.classList.remove("loading");
-    // formButton.textContent = "Save score";
   });
 });
